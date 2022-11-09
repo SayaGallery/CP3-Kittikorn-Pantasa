866ff747d1fb11793a1c478e539604b25e03aab3
SayaGallery <117642839+SayaGallery@users.noreply.github.com>
2022-11-09 15:42:37 +0700

    Create Exercise4_Kittikorn_P.py

---
+++ Exercise4_Kittikorn_P.py
@@ -0,0 +1,13 @@
+Eng = 60.5
+Bus = 70.0
+Ics = 80.0
+Program = 75.0
+
+print("--- Yor Score ---")
+
+print("Foundation Engish",":",Eng)
+print("General Business",":",Bus)
+print("Introduction to Computer Systems",":",Ics)
+print("Computer Programming",":",Program)
+
+print("-----------------");
