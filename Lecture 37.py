5de811ffae476cf28d9eb0ad50ac74e2e268a1a9
SayaGallery <117642839+SayaGallery@users.noreply.github.com>
2022-11-15 01:34:15 +0700

    Add files via upload

---
+++ Lecture 37.py
@@ -0,0 +1,5 @@
+usernameInput = input("Username : ")
+passwordInput = input("Password : ")
+if usernameInput == "admin" and passwordInput == "1234" :
+    print("Success!")
+else: print("Wrong password ):")
